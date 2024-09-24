4baf324d63dad7099c8efed20f8110e04026f07f
ASOST\2316032 <keikocraft87@gmail.com>
2024-09-24 14:41:01 +0900

    3Dshootingをコピペして作ったベース

---
+++ Src/Scene/GameScene.cpp
@@ -0,0 +1,39 @@
+#include <DxLib.h>
+#include "../Application.h"
+#include "../Utility/AsoUtility.h"
+#include "../Manager/ResourceManager.h"
+#include "../Manager/SceneManager.h"
+#include "../Manager/InputManager.h"
+#include "../Manager/Camera.h"
+#include "GameScene.h"
+
+GameScene::GameScene(void)
+{
+}
+
+GameScene::~GameScene(void)
+{
+}
+
+void GameScene::Init(void)
+{
+}
+
+void GameScene::Update(void)
+{
+	// ÉVÅ[ÉìëJà⁄
+	InputManager& ins = InputManager::GetInstance();
+	if (ins.IsTrgDown(KEY_INPUT_SPACE))
+	{
+		SceneManager::GetInstance().ChangeScene(SceneManager::SCENE_ID::TITLE);
+	}
+}
+
+void GameScene::Draw(void)
+{
+	DrawString(0, 0, "GameScene", 0xFFFFFF);
+}
+
+void GameScene::Release(void)
+{
+}
