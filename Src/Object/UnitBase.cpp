5bff00074e21e359f42b824a31e0a76818bea3f9
ratura3939 <158240290+ratura3939@users.noreply.github.com>
2024-10-02 10:44:43 +0900

    add UnitBase

--- Src/Object/UnitBase.cpp
+++ Src/Object/UnitBase.cpp
@@ -1,1 +1,55 @@
 #include "UnitBase.h"
+
+UnitBase::UnitBase(void)
+{
+}
+
+UnitBase::~UnitBase(void)
+{
+}
+
+void UnitBase::Destroy(void)
+{
+}
+
+void UnitBase::Init(void)
+{
+}
+
+void UnitBase::Update(void)
+{
+}
+
+void UnitBase::Draw(void)
+{
+}
+
+bool UnitBase::IsAlive(void) const
+{
+	return hp_ > 0;
+}
+
+VECTOR UnitBase::GetPos(void) const
+{
+	return trans_.pos;
+}
+
+VECTOR UnitBase::GetRot(void) const
+{
+	return trans_.rot;
+}
+
+VECTOR UnitBase::GetScl(void) const
+{
+	return trans_.scl;
+}
+
+float UnitBase::GetAtk(void) const
+{
+	return atk_;
+}
+
+float UnitBase::GetDef(void) const
+{
+	return def_;
+}
--- Src/Object/UnitBase.h
+++ Src/Object/UnitBase.h
@@ -20,10 +20,27 @@ public:
 	//描画
 	virtual void Draw(void);
 
+	//ゲッター各種
+	//生存確認(生存しているとtrue)
+	bool IsAlive(void)const;
+	//位置
+	VECTOR GetPos(void)const;
+	//角度
+	VECTOR GetRot(void)const;
+	//大きさ
+	VECTOR GetScl(void)const;
+	//攻撃力
+	float GetAtk(void)const;
+	//防御力
+	float GetDef(void)const;
+
 private:
 
-	int mdlId_;	//モデル保存
-	int hp_;	//体力
+	int mdlId_;			//モデル保存
+	int hp_;			//体力
+	Transform trans_;	//位置情報関係
+	float atk_;			//攻撃力
+	float def_;			//防御力
 
 };
 
